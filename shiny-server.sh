5202a1280d320c115abbde780bf258055a40f367
Ashish K Sahoo <ashish_k_sahoo@yahoo.co.in>
2020-07-26 04:38:36 +0530

    Signed-off-by: Ashish K Sahoo <ashish_k_sahoo@yahoo.co.in>

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Make sure the directory for individual app logs exists
-mkdir -p /var/log/supervisord/shiny-server
-chown shiny.shiny /var/log/supervisord/shiny-server
+# mkdir -p /var/log/supervisord/shiny-server
+# chown shiny.shiny /var/log/supervisord/shiny-server
 chown shiny.shiny /var/log/shiny-server
 
 if [ "$APPLICATION_LOGS_TO_STDOUT" != "false" ];
 then
     # push the "real" application logs to stdout with xtail in detached mode
-    exec xtail /var/log/supervisord/shiny-server &
+    exec xtail /var/log/shiny-server &
 fi
 
 # start shiny server
