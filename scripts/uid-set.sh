616cbebb62c266973369e8a22d9d2c024ba3db4d
Ashish K Sahoo <ashissah@in.ibm.com>
2020-09-20 07:51:20 +0530

    Access Fix 32
    Signed-off-by: Ashish K Sahoo <ashissah@in.ibm.com>

--- scripts/uid-set.sh
+++ scripts/uid-set.sh
@@ -2,7 +2,7 @@
 
 if [ `id -u` -ge 10000 ]; then
  cat /etc/passwd | sed -e "s/^$user:/builder:/" > /tmp/passwd
- echo "$user:x:`id -u`:`id -g`:,,,:/home/$user:/bin/bash" >> /tmp/passwd
+ echo "$user:x:`id -u`:`id -g`:,,,:/srv/shiny-server:/bin/bash" >> /tmp/passwd
  cat /tmp/passwd > /etc/passwd
  rm /tmp/passwd
  fi
